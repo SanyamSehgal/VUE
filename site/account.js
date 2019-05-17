fcf38a7b791f568c176bbc63d90ca9258334b4ae
Sanyam Sehgal <sanyamsehgal@Vedants-MacBook-Pro.local>
2019-05-18 00:27:21 +0930

    adding files

---
+++ site/account.js
@@ -0,0 +1,22 @@
+var vueinst = new Vue({
+el: '#user',
+  data: {
+    showlogin: false,
+    showlog: false,
+    showsign:false,
+    cross:false,
+    cross1:false
+
+  },
+});
+var vueinst1 = new Vue({
+el: '#RestaurentUser',
+  data: {
+    reslogin: false,
+    ressignup: false,
+    reslog:false,
+    buttoncross:false,
+    buttoncross1:false,
+    
+  },
+});
